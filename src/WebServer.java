4d5761e3935dd27c027cf5ed9bcd5cacb4e52fbe
Joseph-Vanliew <joevanliew@gmail.com>
2024-11-06 17:32:14 -0500

    updated WebServer

--- src/WebServer.java
+++ src/WebServer.java
@@ -4,48 +4,28 @@ import com.sun.net.httpserver.HttpExchange;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class WebServer {
-
     public static void main(String[] args) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8567), 0);
-
-        server.createContext("/", new HtmlHandler());
-
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/hello.html", new HelloHandler());
+        server.setExecutor(null);
         server.start();
-        System.out.println("Server is running on http://localhost:8567/");
+        System.out.println("Server is running on http://localhost:8567/hello.html");
     }
 
-    static class HtmlHandler implements HttpHandler {
+    static class HelloHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
-
-            String path = exchange.getRequestURI().getPath();
-
-            String baseDir = "src";
-
-            File file = new File(baseDir + path);
-
-            if (file.exists() && file.isFile()) {
-                exchange.getResponseHeaders().set("Content-Type", "text/html");
-                exchange.sendResponseHeaders(200, file.length());
-                try (OutputStream os = exchange.getResponseBody(); FileInputStream fs = new FileInputStream(file)) {
-                    byte[] buffer = new byte[1024];
-                    int count;
-                    while ((count = fs.read(buffer)) != -1) {
-                        os.write(buffer, 0, count);
-                    }
-                }
-            } else {
-                String response = "404 (Not Found)\n";
-                exchange.sendResponseHeaders(404, response.length());
-                OutputStream os = exchange.getResponseBody();
-                os.write(response.getBytes());
-                os.close();
-            }
+            String filePath = "hello.html";
+            byte[] response = Files.readAllBytes(Paths.get(filePath));
+            exchange.sendResponseHeaders(200, response.length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(response);
+            os.close();
         }
     }
 }
